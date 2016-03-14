6468007c031da66885627c3e61728c526cba964f
Jesús Sobrino <jesusyyo@gmail.com>
2016-03-14 11:00:50 +0100

    add(dashboard): add dashboard navigation

---
+++ app/ts/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component(
+  {
+    selector: 'dashboard',
+    template: '<h3>Dashboard</h3>'
+  }
+)
+
+export class DashboardComponent{
+
+}
--- app/ts/main/app.component.ts
+++ app/ts/main/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {HeroService} from '../commons/hero.service';
 import {HeroesComponent} from '../heroes/heroes.component';
-import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {DashboardComponent} from '../dashboard/dashboard.component';
+
 
 @Component({
     selector: 'heroes-app',
@@ -10,6 +12,7 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
     template: `
       <h1>{{title}}</h1>
       <a [routerLink]="['Heroes']">Heroes</a>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
       <router-outlet></router-outlet>
   `
 })
@@ -19,6 +22,12 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
     path: '/heroes',
     name: 'Heroes',
     component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
   }
 ])
 
