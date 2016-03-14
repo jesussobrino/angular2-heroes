da7a986f2c22b0e89b72e73f8c7669b76cc8687d
Jesús Sobrino <jesusyyo@gmail.com>
2016-03-14 10:12:21 +0100

    feat(router): add router config navigation

--- app/ts/main/app.component.ts
+++ app/ts/main/app.component.ts
@@ -1,16 +1,26 @@
 import {Component} from 'angular2/core';
-import {HeroService} from "../commons/hero.service";
-import {HeroesComponent} from "../heroes/heroes.component";
+import {HeroService} from '../commons/hero.service';
+import {HeroesComponent} from '../heroes/heroes.component';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
 @Component({
     selector: 'heroes-app',
-    directives: [HeroesComponent],
-    providers: [HeroService],
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS, HeroService],
     template: `
       <h1>{{title}}</h1>
-      <heroes-list></heroes-list>
+      <a [routerLink]="['Heroes']">Heroes</a>
+      <router-outlet></router-outlet>
   `
 })
+
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
 
 export class AppComponent {
     private title = 'Tour of Heroes';
