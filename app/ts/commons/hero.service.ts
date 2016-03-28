26f273cbb8aa63777ae6dd203d3776b12aea3246
Jesús Sobrino <jesusyyo@gmail.com>
2016-03-28 18:00:57 +0200

    feat(*): Rx.Observable example in hero-detail.component

--- app/ts/commons/hero.service.ts
+++ app/ts/commons/hero.service.ts
@@ -1,15 +1,33 @@
 import {Injectable} from 'angular2/core';
+import {URLSearchParams, Jsonp} from 'angular2/http';
+import 'rxjs/add/operator/map';
 import {HEROES} from "./heroes.mock";
 
 @Injectable()
 export class HeroService {
-    getHeroes(){
+    constructor(private jsonp:Jsonp) {
+    }
+
+    getHeroes() {
         return Promise.resolve(HEROES);
     }
 
-    getHero(id:number){
-      return Promise.resolve(HEROES).then(
-        heroes => heroes.filter(hero => hero.id === id)[0]
-      )
+    getHero(id:number) {
+        return Promise.resolve(HEROES).then(
+            heroes => heroes.filter(hero => hero.id === id)[0]
+        )
+    }
+
+    wikiSearch(searchParam:string) {
+        console.log('HeroService:wikiSearch ');
+        
+        let search:URLSearchParams = new URLSearchParams();
+        search.set('action', 'opensearch');
+        search.set('search', searchParam);
+        search.set('format', 'json');
+
+        return this.jsonp
+            .get('http://en.wikipedia.org/w/api.php?callback=JSONP_CALLBACK', {search})
+            .map((request) => request.json()[1]);
     }
 }
--- app/ts/hero-detail/hero-detail.component.ts
+++ app/ts/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
 import {Component, OnInit} from 'angular2/core';
 import {RouteParams} from 'angular2/router';
+import {Control} from 'angular2/common';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
 import {Hero} from '../commons/hero';
 import {HeroService} from '../commons/hero.service';
 import {es_ES} from '../../i18n/es-ES'
@@ -11,20 +16,30 @@ import {es_ES} from '../../i18n/es-ES'
     styleUrls: ['app/ts/hero-detail/hero-detail.component.css']
 })
 
-export class HeroDetailComponent implements OnInit{
-    hero: Hero;
-    esLanguage: any = es_ES;
+export class HeroDetailComponent implements OnInit {
+    hero:Hero;
+    esLanguage:any = es_ES;
+    inputControl:Control = new Control();
+    items:Observable<Array<string>>;
 
-    constructor(private _heroService:HeroService, private _routeParams:RouteParams){
+    constructor(private _heroService:HeroService, private _routeParams:RouteParams) {
     }
 
-    ngOnInit(){
-      var id = parseInt(this._routeParams.get('id'));
-      this._heroService.getHero(id)
-        .then(hero => this.hero = hero);
+    ngOnInit() {
+        var id = parseInt(this._routeParams.get('id'));
+        this._heroService.getHero(id)
+            .then(hero => this.hero = hero);
+        this.getHeroesFromWiki();
     }
 
-    goBack(){
-      window.history.back();
+    getHeroesFromWiki() {
+        this.items = this.inputControl.valueChanges
+            .debounceTime(300)
+            .distinctUntilChanged()
+            .switchMap(inputControlData => this._heroService.wikiSearch(inputControlData.toString()));
+    }
+
+    goBack() {
+        window.history.back();
     }
 }
